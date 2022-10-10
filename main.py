5b341d87fd6f727e1fe3e16f2e90bd55b7d86c24
Ishan Vemireddy <91898895+RedTachyon19@users.noreply.github.com>
2022-10-10 15:54:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import discord
 
 from discord.ext import commands
 
-#from discord_slash import SlashCommand, SlashContext
-#from discord_slash.utils.manage_commands import create_choice, create_option
-
 import keep_alive
 import music
 import os
@@ -13,7 +10,6 @@ import os
 
 bot = commands.Bot(command_prefix= "!c ", intents = discord.Intents.all())
 
-#slash = SlashCommand(bot, sync_commands = True)
 bot.remove_command('help')
 
 cogs = [music]
